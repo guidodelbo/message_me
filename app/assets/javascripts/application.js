c0d685f20ad4e03b5d309ac57f504dc96a83a80f
Guido Delbo <gdelbo@gmail.com>
2022-08-29 19:20:16 -0300

    adds authentication

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,4 +18,7 @@
 //= require_tree .
 $(document).on('turbolinks:load', function () {
   $('.ui.dropdown').dropdown();
+  $('.message .close').on('click', function () {
+    $(this).closest('.message').transition('fade');
+  });
 });
